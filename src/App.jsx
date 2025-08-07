c3c2c1cbabfe408de9ed0c6d733077787508e575
Ahmed <a.jassim2959@gmail.com>
2025-08-07 14:43:34 +0300

    create hoot form

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,60 @@
-// App.jsx
-
-import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { useState, useEffect, createContext } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
 import SignupForm from './components/SignupForm/SignupForm';
 import SigninForm from './components/SigninForm/SigninForm';
+import HootList from './components/HootList/HootList';
+import HootDetails from './components/HootDetails/HootDetails';
+import HootForm from './components/HootForm/HootForm';
 import * as authService from './services/authService';
+import * as hootService from './services/hootService';
 import './App.css';
 
 const App = () => {
+  const navigate = useNavigate();
   const [user, setUser] = useState(null);
+  const [hoots, setHoots] = useState([]);
+
+  useEffect(() => {
+    const fetchAllHoots = async () => {
+      const hootsData = await hootService.index();
+
+      setHoots(hootsData);
+    };
+    if (user) fetchAllHoots();
+  }, [user]);
 
   const handleSignout = () => {
     authService.signout();
     setUser(null);
   };
 
+  const handleAddHoot = async (hootFormData) => {
+  const newHoot = await hootService.create(hootFormData);
+  setHoots([newHoot, ...hoots]);
+  navigate('/hoots');
+};
+
   return (
     <>
       <NavBar user={user} handleSignout={handleSignout} />
       <Routes>
-        {
-          user ?
+        {user ? (
+          // Protected Routes:
+          <>
             <Route path="/" element={<Dashboard user={user} />} />
-            :
-            <Route path="/" element={<Landing />} />
-        }
-        <Route path='/signup' element={<SignupForm setUser={setUser} />} />
-        <Route path='/signin' element={<SigninForm setUser={setUser} />} />
+            <Route path="/hoots" element={<HootList hoots={hoots} />} />
+            <Route path="/hoots/:id" element={<HootDetails hoots={hoots} />} />
+            <Route path="/hoots/new" element={<HootForm handleAddHoot={handleAddHoot} />} />
+          </>
+        ) : (
+          // Public Route:
+          <Route path="/" element={<Landing />} />
+        )}
+        <Route path="/signup" element={<SignupForm setUser={setUser} />} />
+        <Route path="/signin" element={<SigninForm setUser={setUser} />} />
       </Routes>
     </>
   )
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom';
 const NavBar = ({ user, handleSignout }) => {
   return (
     <>
-      { user ? (
+      {user ? (
         <nav>
           <ul>
             <li><Link to="/">Home</Link></li>
+            <li><Link to="/hoots">Hoots</Link></li>
+            <li><Link to="/hoots/new">NEW HOOT</Link></li>
             <li><Link to="" onClick={handleSignout}>Sign Out</Link></li>
           </ul>
         </nav>
@@ -15,6 +17,7 @@ const NavBar = ({ user, handleSignout }) => {
           <ul>
             <li><Link to="/signin">Sign In</Link></li>
             <li><Link to="/signup">Sign Up</Link></li>
+
           </ul>
         </nav>
       )}
